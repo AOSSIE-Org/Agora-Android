e5ce56613c1fa0c58d7ed3f2abb0e831395e5e27
Mukul Kumar <mukul.2121@gmail.com>
2019-07-18 00:22:51 +0530

    Added Splash Screen which is having a rotating agora icon

---
+++ app/src/main/java/org/aossie/agoraandroid/SplashScreen.java
@@ -0,0 +1,47 @@
+package org.aossie.agoraandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.aossie.agoraandroid.main.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Rotation And Fade Out Animation
+
+        final ImageView rotatingAgora = findViewById(R.id.image_agora);
+        final Animation animation1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation animation2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+        rotatingAgora.startAnimation(animation1);
+        animation1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                rotatingAgora.startAnimation(animation2);
+                finish();
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+}
