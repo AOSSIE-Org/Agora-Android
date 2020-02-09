866ec128352621b5453b41d597aae628cb884af3
prudhvi reddy <prudhvireddy.m01@gmail.com>
2020-02-09 18:00:59 +0000

    divided into packages and removeed the redundant test code

--- app/src/test/java/org/aossie/agoraandroid/apitesting/BaseTest.kt
+++ app/src/test/java/org/aossie/agoraandroid/apitesting/BaseTest.kt
@@ -1,29 +1,20 @@
-package org.aossie.agoraandroid
+package org.aossie.agoraandroid.apitesting
 
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
-import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.aossie.agoraandroid.remote.APIService
-import org.aossie.agoraandroid.testserver.Requests
-import org.aossie.agoraandroid.testserver.Responses
 import org.junit.After
-import org.junit.Assert
 import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.runners.MockitoJUnitRunner
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.io.IOException
 
-@RunWith(MockitoJUnitRunner::class)
-class LogInTest {
+open class BaseTest {
 
-  private lateinit var mockWebServer: MockWebServer
-  private lateinit var apiService: APIService
+  lateinit var mockWebServer: MockWebServer
+  lateinit var apiService: APIService
 
   @Before
   @Throws(IOException::class)
@@ -31,7 +22,8 @@ class LogInTest {
     mockWebServer = MockWebServer()
     mockWebServer.start()
     val gson = GsonBuilder().setLenient().create()
-    apiService = Retrofit.Builder().baseUrl(mockWebServer.url("/"))
+    apiService = Retrofit.Builder()
+        .baseUrl(mockWebServer.url("/"))
         .client(OkHttpClient())
         .addConverterFactory(ScalarsConverterFactory.create())
         .addConverterFactory(GsonConverterFactory.create(gson))
@@ -39,17 +31,10 @@ class LogInTest {
         .create(APIService::class.java)
   }
 
-  @Test
-  @Throws(IOException::class)
-  fun LogInUnitTesting() {
-    mockWebServer.enqueue(MockResponse().setBody(Responses.AUTH_LOGIN))
-    val response: Response<*> = apiService.logIn(Requests.AUTH_LOGIN).execute()
-    Assert.assertEquals(response.message(), "OK")
-  }
-
   @After
   @Throws(IOException::class)
   fun teardown() {
     mockWebServer.shutdown()
   }
+
 }
