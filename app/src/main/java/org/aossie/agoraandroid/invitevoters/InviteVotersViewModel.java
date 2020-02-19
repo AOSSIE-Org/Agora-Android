d89d9d8085aff0cca99bef78b7a2072f870864d0
indracs97 <indranil.cse97@gmail.com>
2020-02-20 04:37:14 +0530

    InviteVotersActivity loading fixed and empty VotersActivity has also been fixed.

--- app/src/main/java/org/aossie/agoraandroid/invitevoters/InviteVotersViewModel.java
+++ app/src/main/java/org/aossie/agoraandroid/invitevoters/InviteVotersViewModel.java
@@ -37,11 +37,13 @@ class InviteVotersViewModel extends AndroidViewModel {
     for (int i = 0; i < mVoterEmails.size(); i++) {
       JSONObject jsonObject = new JSONObject();
       jsonObject.put("name", mVoterNames.get(i));
-      jsonObject.put("email", mVoterEmails.get(i));
+      jsonObject.put("hash", mVoterEmails.get(i));
       jsonArray.put(jsonObject);
       Log.d("TAG", "inviteVoters: " + jsonArray);
-      sendVoters(id, token, jsonArray);
     }
+    sendVoters(id, token, jsonArray);
+
+
   }
 
   private void sendVoters(String id, String token, JSONArray jsonArray) {
