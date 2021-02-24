204fcf7fb93ff520973eaf92b799cb977b2edfdb
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-02-24 19:58:36 +0530

    fix #206 Finish Election dates can be before Start Election dates

--- app/src/main/java/org/aossie/agoraandroid/createelection/CreateElectionOne.java
+++ app/src/main/java/org/aossie/agoraandroid/createelection/CreateElectionOne.java
@@ -5,6 +5,7 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -90,14 +91,18 @@ public class CreateElectionOne extends AppCompatActivity {
     });
   }
 
+  DatePickerDialog startDatePickerDialog;
+  TimePickerDialog startTimePickerDialog;
+  Calendar startCalendar;
+
   private void handleStartDateTime() {
-    Calendar calendar = Calendar.getInstance();
-    int YEAR = calendar.get(Calendar.YEAR);
-    int MONTH = calendar.get(Calendar.MONTH);
-    int DATE = calendar.get(Calendar.DATE);
-    int HOUR = calendar.get(Calendar.HOUR);
-    int MINUTE = calendar.get(Calendar.MINUTE);
-    DatePickerDialog datePickerDialog =
+    startCalendar = Calendar.getInstance();
+    int YEAR = startCalendar.get(Calendar.YEAR);
+    int MONTH = startCalendar.get(Calendar.MONTH);
+    int DATE = startCalendar.get(Calendar.DATE);
+    int HOUR = startCalendar.get(Calendar.HOUR);
+    int MINUTE = startCalendar.get(Calendar.MINUTE);
+    startDatePickerDialog =
         new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
           @Override
           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -105,10 +110,14 @@ public class CreateElectionOne extends AppCompatActivity {
             sDay = dayOfMonth;
             sMonth = month;
             sYear = year;
+            startCalendar.set(Calendar.YEAR, year);
+            startCalendar.set(Calendar.MONTH, month);
+            startCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
           }
         }, YEAR, MONTH, DATE);
+    startDatePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
 
-    TimePickerDialog timePickerDialog =
+    startTimePickerDialog =
         new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
           @Override
           public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -127,9 +136,12 @@ public class CreateElectionOne extends AppCompatActivity {
             electionDetailsSharedPrefs.saveStartTime(charSequence.toString());
           }
         }, HOUR, MINUTE, true);
-    timePickerDialog.show();
-    datePickerDialog.show();
+    startTimePickerDialog.show();
+    startDatePickerDialog.show();
   }
+
+  DatePickerDialog endDatePickerDialog;
+  TimePickerDialog endTimePickerDialog;
 
   private void handleEndDateTime() {
     Calendar calendar = Calendar.getInstance();
@@ -138,7 +150,7 @@ public class CreateElectionOne extends AppCompatActivity {
     int DATE = calendar.get(Calendar.DATE);
     int HOUR = calendar.get(Calendar.HOUR);
     int MINUTE = calendar.get(Calendar.MINUTE);
-    DatePickerDialog datePickerDialog =
+    endDatePickerDialog =
         new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
           @Override
           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -149,7 +161,7 @@ public class CreateElectionOne extends AppCompatActivity {
           }
         }, YEAR, MONTH, DATE);
 
-    TimePickerDialog timePickerDialog =
+    endTimePickerDialog =
         new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
           @Override
           public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -168,7 +180,8 @@ public class CreateElectionOne extends AppCompatActivity {
             electionDetailsSharedPrefs.saveEndTime(charSequence2.toString());
           }
         }, HOUR, MINUTE, true);
-    timePickerDialog.show();
-    datePickerDialog.show();
+    endDatePickerDialog.getDatePicker().setMinDate(startCalendar.getTimeInMillis() + 86400000);
+    endTimePickerDialog.show();
+    endDatePickerDialog.show();
   }
 }
