a15d24b63b338faa2fd190a1dc5aaada3c159823
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2021-03-12 21:05:22 +0530

    fixed email validation remove textwatcher

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/signup/SignUpFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/signup/SignUpFragment.kt
@@ -50,6 +50,12 @@ class SignUpFragment
   private var securityQuestionOfSignUp: String? = null
 
   private lateinit var rootView: View
+  private var usernameInput = "";
+  var firstNameInput = "";
+  var lastNameInput = "";
+  var answerInput = "";
+  var emailInput = "";
+  var passwordInput = ""
 
   private val signUpViewModel: SignUpViewModel by viewModels {
     viewModelFactory
@@ -66,7 +72,15 @@ class SignUpFragment
 
     rootView.signup_btn.setOnClickListener {
       HideKeyboard.hideKeyboardInActivity(activity as AppCompatActivity)
-      validateAllFields()
+      if (!Patterns.EMAIL_ADDRESS.matcher(emailInput)
+              .matches()
+      )
+        signup_email.error = "Enter a valid email address!!!"
+      else signUpViewModel.signUpRequest(
+          usernameInput, passwordInput, emailInput, firstNameInput, lastNameInput,
+          securityQuestionOfSignUp!!, answerInput
+      )
+
     }
 
     val adapter = ArrayAdapter.createFromResource(
@@ -104,107 +118,39 @@ class SignUpFragment
     return rootView
   }
 
-  private fun validateAllFields() {
-    val userName = signup_user_name.editText
-        ?.text
-        .toString()
-    val firstName = signup_first_name.editText
-        ?.text
-        .toString()
-    val lastName = signup_last_name.editText
-        ?.text
-        .toString()
-    val userEmail = signup_email.editText
-        ?.text
-        .toString()
-    val userPass = signup_password.editText
-        ?.text
-        .toString()
-    val securityQuestionAnswer = security_answer.editText
-        ?.text
-        .toString()
-    val securityQuestion = securityQuestionOfSignUp
-    validateUsername(userName)
-    if (firstName.isEmpty()) {
-      signup_first_name.error = "Please enter First Name"
-    } else {
-      signup_first_name.error = null
-    }
-    if (lastName.isEmpty()) {
-      signup_last_name.error = "Please enter Last Name"
-    } else {
-      signup_last_name.error = null
-    }
-    if (userEmail.isEmpty()) {
-      signup_email.error = "Please enter Email Address"
-    } else if (!Patterns.EMAIL_ADDRESS.matcher(userEmail)
-            .matches()
-    ) {
-      signup_email.error = "Enter a valid email address!!!"
-      return
-    } else {
-      signup_email.error = null
-    }
-    if (securityQuestionAnswer.isEmpty()) {
-      security_answer.error = "Please enter Security Answer"
-    } else {
-      security_answer.error = null
-    }
-    validatePassword(
-        userName, firstName, lastName, userEmail, userPass, securityQuestionAnswer,
-        securityQuestion
-    )
-  }
-
-  private fun validatePassword(
-    userName: String,
-    firstName: String,
-    lastName: String,
-    userEmail: String,
-    userPass: String,
-    securityQuestionAnswer: String,
-    securityQuestion: String?
-  ) {
-    if (userPass.isEmpty()) {
-      signup_password.error = "Please enter password"
-    } else {
-      signup_password.error = null
-      signUpViewModel.signUpRequest(
-          userName, userPass, userEmail, firstName, lastName,
-          securityQuestion!!, securityQuestionAnswer
-      )
-    }
-  }
-
-  private fun validateUsername(userName: String) {
-    if (userName.isEmpty()) {
-      signup_user_name.error = "Please enter User Name"
-    } else {
-      signup_user_name.error = null
-    }
-  }
-
   private val signUpTextWatcher: TextWatcher = object : TextWatcher {
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+    override fun beforeTextChanged(
+      s: CharSequence,
+      start: Int,
+      count: Int,
+      after: Int
+    ) {
+    }
+
     override fun afterTextChanged(s: Editable) {}
 
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-      val usernameInput: String = rootView.et_username.text
+    override fun onTextChanged(
+      s: CharSequence,
+      start: Int,
+      before: Int,
+      count: Int
+    ) {
+      usernameInput = rootView.et_username.text
           .toString()
           .trim()
-      val passwordInput: String = rootView.et_password.text
+      passwordInput = rootView.et_password.text
           .toString()
           .trim()
-      val firstNameInput: String = rootView.et_first_name.text
+      firstNameInput = rootView.et_first_name.text
           .toString()
           .trim()
-      val lastNameInput: String = rootView.et_last_name.text
+      lastNameInput = rootView.et_last_name.text
           .toString()
           .trim()
-      val emailInput: String = rootView.et_email.text
+      emailInput = rootView.et_email.text
           .toString()
           .trim()
-      val answerInput: String = rootView.et_answer.text
+      answerInput = rootView.et_answer.text
           .toString()
           .trim()
       rootView.signup_btn.isEnabled = usernameInput.isNotEmpty()
