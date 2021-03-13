54a1f78d69f8f707dd0ce0761cd847ffebe1b18c
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2021-03-13 12:19:14 +0530

    fixed frequent login issue

--- app/src/main/java/org/aossie/agoraandroid/data/network/interceptors/AuthorizationInterceptor.kt
+++ app/src/main/java/org/aossie/agoraandroid/data/network/interceptors/AuthorizationInterceptor.kt
@@ -22,7 +22,7 @@ class AuthorizationInterceptor(
   override fun intercept(chain: Interceptor.Chain): Response {
     val mainResponse = chain.proceed(chain.request())
 
-    if (prefs.getIsLoggedIn()) {
+
       // if response code is 401 or 403, network call has encountered authentication error
       if (mainResponse.code() == 401 || mainResponse.code() == 403) {
         Coroutines.io{
@@ -59,7 +59,7 @@ class AuthorizationInterceptor(
         }
         throw SessionExpirationException("Your session was expired. Please try again")
       }
-    }
+
     return mainResponse
   }
 }
