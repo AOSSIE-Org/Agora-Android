6de4f37574f4de27d1cf7173353e9e96fecc8bae
divyank00 <lunkaddivyank@gmail.com>
2021-03-28 11:00:45 +0530

    fix: 315 Replaced var to val in Winner,kt

--- app/src/main/java/org/aossie/agoraandroid/data/network/responses/Winners.kt
+++ app/src/main/java/org/aossie/agoraandroid/data/network/responses/Winners.kt
@@ -3,7 +3,7 @@ package org.aossie.agoraandroid.data.network.responses
 import org.aossie.agoraandroid.data.db.model.Winner
 
 data class Winners(
-  var winners: List<Winner>
+  val winners: List<Winner>
 )
 
 
