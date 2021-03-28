ac3403bd96654cdad46a93d4502fb2ca39aa0bdb
danishjamal104 <danishjamal.104@gmail.com>
2021-03-28 18:32:44 +0000

    View refrence removed from ResultViewModel
    Removed unused imports

--- app/src/main/java/org/aossie/agoraandroid/result/ResultViewModel.java
+++ app/src/main/java/org/aossie/agoraandroid/result/ResultViewModel.java
@@ -3,11 +3,12 @@ package org.aossie.agoraandroid.result;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import org.aossie.agoraandroid.R;
 import org.aossie.agoraandroid.remote.APIService;
 import org.aossie.agoraandroid.remote.RetrofitClient;
+import org.aossie.agoraandroid.ui.fragments.electionDetails.ResultFetchFailureListener;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,10 +18,12 @@ import retrofit2.Response;
 
 public class ResultViewModel extends AndroidViewModel {
   private final Context context;
+  private ResultFetchFailureListener electionResultListener;
 
-  public ResultViewModel(@NonNull Application application, Context context) {
+  public ResultViewModel(@NonNull Application application, Context context, ResultFetchFailureListener failureListener) {
     super(application);
     this.context = context;
+    this.electionResultListener = failureListener;
   }
 
   public void getResult(String token, String id) {
@@ -49,14 +52,13 @@ public class ResultViewModel extends AndroidViewModel {
             e.printStackTrace();
           }
         } else if (response.message().equals("No Content")) {
-          Toast.makeText(getApplication(), "Nothing to show here", Toast.LENGTH_SHORT).show();
+          electionResultListener.onResultFetchMessage(R.string.nothing_to_show_here);
         }
       }
 
       @Override
       public void onFailure(Call<String> call, Throwable t) {
-        Toast.makeText(getApplication(), "Something Went Wrong Please Try Again Later",
-            Toast.LENGTH_SHORT).show();
+        electionResultListener.onResultFetchMessage(R.string.something_went_wrong_please_try_again_later);
       }
     });
   }
