eacbc2558545f802a85ea901cea4abb56d360def
divyank00 <lunkaddivyank@gmail.com>
2021-05-27 21:21:43 +0530

    Converted rootView to Binding

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/CalendarViewElectionFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/CalendarViewElectionFragment.kt
@@ -7,30 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.DrawableRes
-import androidx.core.widget.NestedScrollView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.google.gson.Gson
-import com.linkedin.android.tachyon.DayView
 import com.linkedin.android.tachyon.DayView.EventTimeRange
 import devs.mulham.horizontalcalendar.HorizontalCalendar
 import devs.mulham.horizontalcalendar.HorizontalCalendar.Builder
 import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener
-import kotlinx.android.synthetic.main.content_calendar_view.view.swipe_refresh
-import kotlinx.android.synthetic.main.fragment_calendar_view_election.view.calendarView
-import kotlinx.android.synthetic.main.fragment_calendar_view_election.view.fab_list_view
-import kotlinx.android.synthetic.main.fragment_calendar_view_election.view.img_btn_month
-import kotlinx.android.synthetic.main.fragment_calendar_view_election.view.progress_bar
 import org.aossie.agoraandroid.R
 import org.aossie.agoraandroid.R.color
 import org.aossie.agoraandroid.R.layout
 import org.aossie.agoraandroid.data.db.PreferenceProvider
 import org.aossie.agoraandroid.data.db.entities.Election
+import org.aossie.agoraandroid.databinding.FragmentCalendarViewElectionBinding
 import org.aossie.agoraandroid.utilities.hide
 import timber.log.Timber
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.ArrayList
 import java.util.Calendar
@@ -46,43 +42,44 @@ constructor(
   private val prefs: PreferenceProvider
 ) : Fragment() {
 
-  lateinit var rootView: View
-
   private val electionViewModel: ElectionViewModel by viewModels {
     viewModelFactory
   }
+
+  private lateinit var binding: FragmentCalendarViewElectionBinding
 
   private var horizontalCalendar: HorizontalCalendar? = null
 
   private var day: Calendar? = null
   private var allEvents: HashMap<Long, List<Event>>? =
     null
-  private var dateFormat: java.text.DateFormat? = null
-  private var timeFormat: java.text.DateFormat? = null
-
-  private var content: ViewGroup? = null
-  private var dateTextView: TextView? = null
-  private var scrollView: NestedScrollView? = null
-  private var dayView: DayView? = null
+  private var dateFormat: DateFormat? = null
+  private var timeFormat: DateFormat? = null
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    rootView = inflater.inflate(layout.fragment_calendar_view_election, container, false)
-    rootView.swipe_refresh.setColorSchemeResources(color.logo_yellow, color.logo_green)
+    binding =
+      DataBindingUtil.inflate(inflater, layout.fragment_calendar_view_election, container, false)
 
-    rootView.calendarView.visibility = View.GONE
-    rootView.img_btn_month.setOnClickListener {
-      if (rootView.calendarView.visibility == View.GONE) {
-        rootView.calendarView.visibility = View.VISIBLE
-        it.animate().rotation(180F).start()
-      } else if (rootView.calendarView.visibility == View.VISIBLE) {
-        rootView.calendarView.visibility = View.GONE
-        it.animate().rotation(360F).start()
-      }
-    }
+    initView()
+
+    initObjects()
+
+    initListeners()
+
+    return binding.root
+  }
+
+  private fun initView() {
+    binding.calendarLayout.swipeRefresh.setColorSchemeResources(color.logo_yellow, color.logo_green)
+
+    binding.calendarView.visibility = View.GONE
+  }
+
+  private fun initObjects() {
 
     day = Calendar.getInstance()
     day?.set(Calendar.HOUR_OF_DAY, 0)
@@ -93,27 +90,24 @@ constructor(
     allEvents = HashMap()
 
     dateFormat = SimpleDateFormat("MMM yyyy", Locale.ENGLISH)
-    timeFormat = java.text.DateFormat.getTimeInstance(
-      java.text.DateFormat.SHORT, Locale.getDefault()
+    timeFormat = DateFormat.getTimeInstance(
+      DateFormat.SHORT, Locale.getDefault()
     )
-
-    content = rootView.findViewById(R.id.sample_content)
-    dateTextView = rootView.findViewById(R.id.sample_date)
-    scrollView = rootView.findViewById(R.id.sample_scroll)
-    dayView = rootView.findViewById(R.id.sample_day)
 
     val hour = day!!.clone() as Calendar
     val hourLabelViews: MutableList<View> =
       ArrayList()
-    for (i in dayView!!.startHour..dayView!!.endHour) {
+    for (i in binding.calendarLayout.sampleDay.startHour..binding.calendarLayout.sampleDay.endHour) {
       hour[Calendar.HOUR_OF_DAY] = i
       val hourLabelView =
-        layoutInflater.inflate(layout.hour_label, dayView, false) as TextView
+        layoutInflater.inflate(
+          layout.hour_label, binding.calendarLayout.sampleDay, false
+        ) as TextView
       hourLabelView.text = timeFormat!!.format(hour.time)
       hourLabelViews.add(hourLabelView)
     }
-    dayView!!.setHourLabelViews(hourLabelViews)
-    dateTextView!!.text = dateFormat!!.format(day!!.time)
+    binding.calendarLayout.sampleDay.setHourLabelViews(hourLabelViews)
+    binding.sampleDate.text = dateFormat!!.format(day!!.time)
 
     val startDate = Calendar.getInstance()
     startDate.add(Calendar.YEAR, -10)
@@ -123,7 +117,7 @@ constructor(
 
     val defaultSelectedDate = Calendar.getInstance()
 
-    horizontalCalendar = Builder(rootView, R.id.horizontal_calendarView)
+    horizontalCalendar = Builder(binding.root, R.id.horizontal_calendarView)
       .range(startDate, endDate)
       .datesNumberOnScreen(5)
       .configure()
@@ -146,60 +140,77 @@ constructor(
       ) {
         day = date
         onDayChange()
-        rootView.calendarView.date = date.timeInMillis
-        scrollView!!.smoothScrollTo(0, dayView!!.firstEventTop)
+        binding.calendarView.date = date.timeInMillis
+        binding.calendarLayout.sampleScroll.smoothScrollTo(
+          0, binding.calendarLayout.sampleDay.firstEventTop
+        )
+      }
+    }
+  }
+
+  private fun initListeners() {
+    binding.imgBtnMonth.setOnClickListener {
+      if (binding.calendarView.visibility == View.GONE) {
+        binding.calendarView.visibility = View.VISIBLE
+        it.animate()
+          .rotation(180F)
+          .start()
+      } else if (binding.calendarView.visibility == View.VISIBLE) {
+        binding.calendarView.visibility = View.GONE
+        it.animate()
+          .rotation(360F)
+          .start()
       }
     }
 
-    rootView.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+    binding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
       val cal = Calendar.getInstance()
       cal.set(Calendar.YEAR, year)
       cal.set(Calendar.MONTH, month)
       cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-      dateTextView!!.text = dateFormat!!.format(cal.time)
+      binding.sampleDate.text = dateFormat!!.format(cal.time)
       horizontalCalendar!!.selectDate(cal, false)
     }
 
-    rootView.swipe_refresh.setOnRefreshListener { doYourUpdate() }
+    binding.calendarLayout.swipeRefresh.setOnRefreshListener { doYourUpdate() }
 
-    electionViewModel.getElections().observe(
-      viewLifecycleOwner,
-      Observer {
-        if (it != null) {
-          for (election in it) {
-            addEvent(election)
-            onDayChange()
+    electionViewModel.getElections()
+      .observe(
+        viewLifecycleOwner,
+        Observer {
+          if (it != null) {
+            for (election in it) {
+              addEvent(election)
+              onDayChange()
+            }
           }
         }
-      }
-    )
+      )
 
-    rootView.fab_list_view.setOnClickListener {
+    binding.fabListView.setOnClickListener {
       Navigation
-        .findNavController(rootView)
+        .findNavController(binding.root)
         .navigate(
           CalendarViewElectionFragmentDirections
             .actionCalendarViewElectionFragmentToElectionsFragment()
         )
     }
-
-    return rootView
   }
 
   override fun onDestroyView() {
-    rootView.swipe_refresh.setOnRefreshListener(null)
+    binding.calendarLayout.swipeRefresh.setOnRefreshListener(null)
 
     super.onDestroyView()
   }
 
   private fun doYourUpdate() {
     prefs.setUpdateNeeded(true)
-    Navigation.findNavController(rootView)
+    Navigation.findNavController(binding.root)
       .navigate(R.id.calendarViewElectionFragment)
   }
 
   private fun onDayChange() {
-    dateTextView!!.text = dateFormat!!.format(day!!.time)
+    binding.sampleDate.text = dateFormat!!.format(day!!.time)
     if (activity != null) {
       onEventsChange()
     }
@@ -210,7 +221,8 @@ constructor(
     var eventTimeRanges: MutableList<EventTimeRange?>? = null
     val events = allEvents!![day!!.timeInMillis]
     val gson = Gson()
-    Timber.tag("on Event Change").d(gson.toJson(events) + " , day time in millis : " + day!!.timeInMillis)
+    Timber.tag("on Event Change")
+      .d(gson.toJson(events) + " , day time in millis : " + day!!.timeInMillis)
     if (events != null) {
       Collections.sort(
         events
@@ -218,16 +230,17 @@ constructor(
       eventViews = ArrayList()
       eventTimeRanges = ArrayList()
 
-      val recycled = dayView!!.removeEventViews()
+      val recycled = binding.calendarLayout.sampleDay.removeEventViews()
       var remaining = recycled?.size ?: 0
       for (event in events) {
         val eventView =
           if (remaining > 0) recycled!![--remaining] else layoutInflater.inflate(
-            layout.list_item_event, dayView, false
+            layout.list_item_event, binding.calendarLayout.sampleDay, false
           )
         val status = "Status : ${event.status}"
         (eventView.findViewById<View>(R.id.tv_event_title) as TextView).text = event.title
-        (eventView.findViewById<View>(R.id.tv_event_description) as TextView).text = event.description
+        (eventView.findViewById<View>(R.id.tv_event_description) as TextView).text =
+          event.description
         (eventView.findViewById<View>(R.id.tv_event_status) as TextView).text = status
         eventView.background = resources.getDrawable(event.color, resources.newTheme())
 
@@ -235,7 +248,7 @@ constructor(
           val action =
             CalendarViewElectionFragmentDirections
               .actionCalendarViewElectionFragmentToElectionDetailsFragment(event.id)
-          Navigation.findNavController(rootView)
+          Navigation.findNavController(binding.root)
             .navigate(action)
         }
 
@@ -245,12 +258,14 @@ constructor(
         eventTimeRanges.add(EventTimeRange(startMinute, endMinute.toInt()))
       }
     }
-    dayView!!.setEventViews(eventViews, eventTimeRanges)
-    scrollView!!.post {
-      scrollView!!.smoothScrollTo(0, dayView!!.firstEventTop - 300)
+    binding.calendarLayout.sampleDay.setEventViews(eventViews, eventTimeRanges)
+    binding.calendarLayout.sampleScroll.post {
+      binding.calendarLayout.sampleScroll.smoothScrollTo(
+        0, binding.calendarLayout.sampleDay.firstEventTop - 300
+      )
     }
-    if (rootView.progress_bar.visibility == View.VISIBLE) {
-      rootView.progress_bar.hide()
+    if (binding.progressBar.visibility == View.VISIBLE) {
+      binding.progressBar.hide()
     }
   }
 
@@ -260,12 +275,15 @@ constructor(
     val formattedEndingDate: Date? = formatter.parse(election.end!!)
     val currentDate = Calendar.getInstance()
       .time
-    var status: String ? = null
+    var status: String? = null
     var eventColor = R.drawable.cornered_blue_background
     if (currentDate.before(formattedStartingDate)) {
       eventColor = R.drawable.cornered_green_background
       status = "PENDING"
-    } else if (currentDate.after(formattedStartingDate) && currentDate.before(formattedEndingDate)) {
+    } else if (currentDate.after(formattedStartingDate) && currentDate.before(
+        formattedEndingDate
+      )
+    ) {
       eventColor = R.drawable.cornered_red_background
       status = "ACTIVE"
     } else if (currentDate.after(formattedEndingDate)) {
@@ -296,7 +314,8 @@ constructor(
       events.add(Event(id, title, description, status, hour, minute, duration, eventColor))
       allEvents!![sDate.timeInMillis] = events
       val gson = Gson()
-      Timber.tag("all Events").d(gson.toJson(allEvents))
+      Timber.tag("all Events")
+        .d(gson.toJson(allEvents))
       startCalendar.add(Calendar.DATE, 1)
       startCalendar.set(Calendar.HOUR, 0)
       startCalendar.set(Calendar.MINUTE, 0)
