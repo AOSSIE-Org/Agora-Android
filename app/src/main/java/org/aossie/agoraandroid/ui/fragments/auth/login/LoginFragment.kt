2b16cece6ad9847dc30d4028427c089a9ecc2599
Divyank Lunkad <lunkaddivyank@gmail.com>
2021-06-06 01:16:55 +0000

    fix: #267 Disabled facebook button once clicked

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/login/LoginFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/login/LoginFragment.kt
@@ -2,12 +2,11 @@ package org.aossie.agoraandroid.ui.fragments.auth.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -19,18 +18,13 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
-import kotlinx.android.synthetic.main.fragment_login.view.btn_facebook_login
-import kotlinx.android.synthetic.main.fragment_login.view.forgot_password_tv
-import kotlinx.android.synthetic.main.fragment_login.view.login_btn
-import kotlinx.android.synthetic.main.fragment_login.view.login_password_til
-import kotlinx.android.synthetic.main.fragment_login.view.login_user_name_til
-import kotlinx.android.synthetic.main.fragment_login.view.password
-import kotlinx.android.synthetic.main.fragment_login.view.progress_bar
-import kotlinx.android.synthetic.main.fragment_login.view.username
 import org.aossie.agoraandroid.R
 import org.aossie.agoraandroid.data.db.PreferenceProvider
+import org.aossie.agoraandroid.databinding.FragmentLoginBinding
 import org.aossie.agoraandroid.ui.fragments.auth.AuthListener
 import org.aossie.agoraandroid.utilities.HideKeyboard
+import org.aossie.agoraandroid.utilities.disableView
+import org.aossie.agoraandroid.utilities.enableView
 import org.aossie.agoraandroid.utilities.hide
 import org.aossie.agoraandroid.utilities.show
 import org.aossie.agoraandroid.utilities.snackbar
@@ -48,11 +42,11 @@ constructor(
   private val prefs: PreferenceProvider
 ) : Fragment(), AuthListener, LoginListener {
 
+  private lateinit var binding: FragmentLoginBinding
+
   private val loginViewModel: LoginViewModel by viewModels {
     viewModelFactory
   }
-
-  private lateinit var rootView: View
 
   private var callbackManager: CallbackManager? = null
 
@@ -62,8 +56,16 @@ constructor(
     savedInstanceState: Bundle?
   ): View? {
     // Inflate the layout for this fragment
-    rootView = inflater.inflate(R.layout.fragment_login, container, false)
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
+    initObjects()
+
+    initListeners()
+
+    return binding.root
+  }
+
+  private fun initObjects() {
     loginViewModel.authListener = this
     loginViewModel.loginListener = this
 
@@ -80,28 +82,30 @@ constructor(
           }
 
           override fun onCancel() {
-            Toast.makeText(context, "Login Cancel", Toast.LENGTH_LONG)
-              .show()
+            enableBtnFacebook()
+            binding.root.snackbar(resources.getString(R.string.login_cancelled))
           }
 
           override fun onError(exception: FacebookException) {
-            Toast.makeText(context, exception.message, Toast.LENGTH_LONG)
-              .show()
+            enableBtnFacebook()
+            binding.root.snackbar(exception.message.toString())
           }
         }
       )
+  }
 
-    rootView.forgot_password_tv.setOnClickListener {
-      Navigation.findNavController(rootView)
+  private fun initListeners() {
+    binding.forgotPasswordTv.setOnClickListener {
+      Navigation.findNavController(binding.root)
         .navigate(LoginFragmentDirections.actionLoginFragmentToForgotPasswordFragment())
     }
 
-    rootView.login_btn.setOnClickListener {
-      val userName = rootView.login_user_name_til.editText
+    binding.loginBtn.setOnClickListener {
+      val userName = binding.loginUserNameTil.editText
         ?.text
         .toString()
         .trim { it <= ' ' }
-      val userPass = rootView.login_password_til.editText
+      val userPass = binding.loginPasswordTil.editText
         ?.text
         .toString()
         .trim { it <= ' ' }
@@ -109,33 +113,31 @@ constructor(
       loginViewModel.logInRequest(userName, userPass)
     }
 
-    rootView.password.addTextChangedListener(loginTextWatcher)
-    rootView.username.addTextChangedListener(loginTextWatcher)
+    binding.password.doAfterTextChanged {
+      updateLoginButton()
+    }
+    binding.username.doAfterTextChanged {
+      updateLoginButton()
+    }
 
-    rootView.btn_facebook_login.setOnClickListener {
+    binding.btnFacebookLogin.setOnClickListener {
+      disableBtnFacebook()
       LoginManager.getInstance()
         .logInWithReadPermissions(
           activity,
           listOf("email", "public_profile")
         )
     }
-
-    return rootView
   }
 
-  private val loginTextWatcher: TextWatcher = object : TextWatcher {
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-    override fun afterTextChanged(s: Editable) {}
-
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-      val usernameInput: String = rootView.username.text
-        .toString()
-        .trim()
-      val passwordInput: String = rootView.password.text
-        .toString()
-        .trim()
-      rootView.login_btn.isEnabled = usernameInput.isNotEmpty() && passwordInput.isNotEmpty()
-    }
+  private fun updateLoginButton() {
+    val usernameInput: String = binding.username.text
+      .toString()
+      .trim()
+    val passwordInput: String = binding.password.text
+      .toString()
+      .trim()
+    binding.loginBtn.isEnabled = usernameInput.isNotEmpty() && passwordInput.isNotEmpty()
   }
 
   override fun onActivityResult(
@@ -148,36 +150,50 @@ constructor(
   }
 
   override fun onSuccess(message: String?) {
-    rootView.login_btn.toggleIsEnable()
-    rootView.progress_bar.hide()
-    Navigation.findNavController(rootView)
+    binding.loginBtn.toggleIsEnable()
+    binding.progressBar.hide()
+    Navigation.findNavController(binding.root)
       .navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
   }
 
   override fun onStarted() {
-    rootView.progress_bar.show()
-    rootView.login_btn.toggleIsEnable()
+    binding.progressBar.show()
+    binding.loginBtn.toggleIsEnable()
   }
 
   override fun onFailure(message: String) {
-    rootView.progress_bar.hide()
-    rootView.snackbar(message)
-    rootView.login_btn.toggleIsEnable()
+    binding.progressBar.hide()
+    binding.root.snackbar(message)
+    binding.loginBtn.toggleIsEnable()
+    enableBtnFacebook()
   }
 
-  override fun onTwoFactorAuthentication(password: String, crypto: String) {
-    loginViewModel.getLoggedInUser().observe(
-      viewLifecycleOwner,
-      Observer {
-        if (it != null) {
-          if (it.twoFactorAuthentication!!) {
-            rootView.snackbar("OTP is sent to your registered email address")
-            val action = LoginFragmentDirections.actionLoginFragmentToTwoFactorAuthFragment(password, crypto)
-            Navigation.findNavController(rootView)
-              .navigate(action)
+  private fun enableBtnFacebook() {
+    binding.btnFacebookLogin.enableView()
+  }
+
+  private fun disableBtnFacebook() {
+    binding.btnFacebookLogin.disableView()
+  }
+
+  override fun onTwoFactorAuthentication(
+    password: String,
+    crypto: String
+  ) {
+    loginViewModel.getLoggedInUser()
+      .observe(
+        viewLifecycleOwner,
+        Observer {
+          if (it != null) {
+            if (it.twoFactorAuthentication!!) {
+              binding.root.snackbar(getString(R.string.otp_sent))
+              val action =
+                LoginFragmentDirections.actionLoginFragmentToTwoFactorAuthFragment(password, crypto)
+              Navigation.findNavController(binding.root)
+                .navigate(action)
+            }
           }
         }
-      }
-    )
+      )
   }
 }
