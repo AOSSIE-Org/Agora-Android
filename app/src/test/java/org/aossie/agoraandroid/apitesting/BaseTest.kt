0d19e9ffda07faee31757d1eda7b6808af05f596
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2021-06-11 15:53:05 +0530

    removed scalar from BaseTest

--- app/src/test/java/org/aossie/agoraandroid/apitesting/BaseTest.kt
+++ app/src/test/java/org/aossie/agoraandroid/apitesting/BaseTest.kt
@@ -8,7 +8,6 @@ import org.junit.After
 import org.junit.Before
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.io.IOException
 
 open class BaseTest {
@@ -27,7 +26,6 @@ open class BaseTest {
     apiService = Retrofit.Builder()
       .baseUrl(mockWebServer.url("/"))
       .client(OkHttpClient())
-      .addConverterFactory(ScalarsConverterFactory.create())
       .addConverterFactory(MoshiConverterFactory.create())
       .build()
       .create(Api::class.java)
