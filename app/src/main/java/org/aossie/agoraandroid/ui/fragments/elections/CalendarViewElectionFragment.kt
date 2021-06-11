6ebcdea3001f3c865272bd67cbf90b110758c3c9
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2021-06-11 08:50:47 +0530

    final commit

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/CalendarViewElectionFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/CalendarViewElectionFragment.kt
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import com.google.gson.Gson
 import com.linkedin.android.tachyon.DayView.EventTimeRange
 import devs.mulham.horizontalcalendar.HorizontalCalendar
 import devs.mulham.horizontalcalendar.HorizontalCalendar.Builder
@@ -250,9 +249,6 @@ constructor(
     var eventViews: MutableList<View?>? = null
     var eventTimeRanges: MutableList<EventTimeRange?>? = null
     val events = allEvents!![day!!.timeInMillis]
-    val gson = Gson()
-    Timber.tag("on Event Change")
-      .d(gson.toJson(events) + " , day time in millis : " + day!!.timeInMillis)
     if (events != null) {
       Collections.sort(
         events
@@ -366,9 +362,6 @@ constructor(
         (endCalendar.timeInMillis - startCalendar.timeInMillis) / 60000
       events.add(Event(id, title, description, status, hour, minute, duration, eventColor))
       allEvents!![sDate.timeInMillis] = events
-      val gson = Gson()
-      Timber.tag("all Events")
-        .d(gson.toJson(allEvents))
       startCalendar.add(Calendar.DATE, 1)
       startCalendar.set(Calendar.HOUR, 0)
       startCalendar.set(Calendar.MINUTE, 0)
