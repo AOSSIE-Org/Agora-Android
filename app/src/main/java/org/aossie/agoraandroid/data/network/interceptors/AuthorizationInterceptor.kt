2216bc1e5496a386c217311230341e2a9370e7e5
divyank00 <lunkaddivyank@gmail.com>
2021-06-14 13:20:33 +0530

    fix: #330 Merged base branch

--- app/src/main/java/org/aossie/agoraandroid/data/network/interceptors/AuthorizationInterceptor.kt
+++ app/src/main/java/org/aossie/agoraandroid/data/network/interceptors/AuthorizationInterceptor.kt
@@ -25,47 +25,18 @@ class AuthorizationInterceptor(
   override fun intercept(chain: Interceptor.Chain): Response {
     val request = chain.request()
     val mainResponse = chain.proceed(request)
-    var tryCount = 3 // 3 retries
+    var tryCount = AppConstants.retryCount
 
     // if response code is 401, network call has encountered authentication error
     while (mainResponse.code == AppConstants.UNAUTHENTICATED_CODE) {
       if (prefs.getIsLoggedIn()) {
-        var newResponse: Response
         runBlocking {
           if (tryCount == 0) {
             throw SessionExpirationException()
           }
           tryCount--
-          var user = appDatabase.getUserDao().getUserInfo()
-          if (prefs.getIsFacebookUser()) {
-            val response = api.facebookLogin()
-            if (response.isSuccessful) {
-              // save new access token
-              prefs.setCurrentToken(response.body()!!.authToken?.token)
-              user.token = response.body()!!.authToken?.token
-              user.expiredAt = response.body()!!.authToken?.expiresOn
-            } else {
-              throw SessionExpirationException()
-            }
-          } else {
-
-            val loginResponse = api.logIn(LoginDto(user.username ?: "", user.trustedDevice ?: "", user.password ?: ""))
-            if (loginResponse.isSuccessful) {
-              val authResponse: AuthResponse? = loginResponse.body()
-              authResponse.let {
-                user = User(
-                  it?.username, it?.email, it?.firstName, it?.lastName, it?.avatarURL,
-                  it?.crypto, it?.twoFactorAuthentication,
-                  it?.authToken?.token, it?.authToken?.expiresOn, user.password, user.trustedDevice
-                )
-                Timber.d(authResponse.toString())
-              }
-            } else {
-              throw SessionExpirationException()
-            }
-          }
-          appDatabase.getUserDao().replace(user)
-          prefs.setCurrentToken(user.token)
+          val newToken = renewTokenAndUpdateUser()
+          prefs.setCurrentToken(newToken)
         }
         mainResponse.close()
         return chain.proceed(updateRequestWithToken(request))
@@ -76,7 +47,44 @@ class AuthorizationInterceptor(
     return mainResponse
   }
 
+  private suspend fun renewTokenAndUpdateUser() : String? {
+    var user = appDatabase.getUserDao()
+      .getUserInfo()
+    if (prefs.getIsFacebookUser()) {
+      val response = api.facebookLogin()
+      if (response.isSuccessful) {
+        // save new access token
+        prefs.setCurrentToken(response.body()!!.authToken?.token)
+        user.token = response.body()!!.authToken?.token
+        user.expiredAt = response.body()!!.authToken?.expiresOn
+      } else {
+        throw SessionExpirationException()
+      }
+    } else {
+      val loginResponse =
+        api.logIn(LoginDto(user.username ?: "", user.trustedDevice ?: "", user.password ?: ""))
+      if (loginResponse.isSuccessful) {
+        val authResponse: AuthResponse? = loginResponse.body()
+        authResponse.let {
+          user = User(
+            it?.username, it?.email, it?.firstName, it?.lastName, it?.avatarURL,
+            it?.crypto, it?.twoFactorAuthentication,
+            it?.authToken?.token, it?.authToken?.expiresOn, user.password, user.trustedDevice
+          )
+          Timber.d(authResponse.toString())
+        }
+      } else {
+        throw SessionExpirationException()
+      }
+    }
+    appDatabase.getUserDao()
+      .replace(user)
+    return user.token
+  }
+
   private fun updateRequestWithToken(request: Request): Request {
-    return request.newBuilder().header("X-Auth-Token", prefs.getCurrentToken() ?: "").build()
+    return request.newBuilder()
+      .header(AppConstants.X_AUTH_TOKEN, prefs.getCurrentToken() ?: "")
+      .build()
   }
 }
--- app/src/main/java/org/aossie/agoraandroid/utilities/AppConstants.kt
+++ app/src/main/java/org/aossie/agoraandroid/utilities/AppConstants.kt
@@ -3,13 +3,13 @@ package org.aossie.agoraandroid.utilities
 object AppConstants {
   const val ok = "OK"
   const val undo = "UNDO"
-  const val BAD_REQUEST_CODE = 400
   const val FACEBOOK = "facebook"
   const val X_AUTH_TOKEN = "X-Auth-Token"
   const val ACCESS_TOKEN = "Access-Token"
   const val ACCEPT = "Accept"
   const val CONTENT_TYPE = "Content-Type"
   const val APPLICATION_JSON = "application/json"
+  const val BAD_REQUEST_CODE = 400
   const val UNAUTHENTICATED_CODE = 401
   const val INVALID_CREDENTIALS_CODE = 403
   const val INTERNAL_SERVER_ERROR_CODE = 500
@@ -21,4 +21,5 @@ object AppConstants {
   const val UPVOTED_CANDIDATE_ITEM_CLICKED = 1
   const val URL_REGEX = "^((https?|ftp)://|(www|ftp)\\.)?[a-z0-9-]+(\\.[a-z0-9-]+)+([/?].*)?$"
   const val BASE_URL = "https://agora-rest-api.herokuapp.com/api/v1/"
+  const val retryCount = 3
 }
