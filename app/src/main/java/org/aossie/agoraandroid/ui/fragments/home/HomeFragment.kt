fb7fc5ed9ac6ac75d9226a7f6908e512a5cf39ae
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2021-07-08 19:43:34 +0530

    remove nested observer

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/home/HomeFragment.kt
@@ -10,26 +10,12 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_home.view.button_create_election
-import kotlinx.android.synthetic.main.fragment_home.view.card_view_active_elections
-import kotlinx.android.synthetic.main.fragment_home.view.card_view_finished_elections
-import kotlinx.android.synthetic.main.fragment_home.view.card_view_pending_elections
-import kotlinx.android.synthetic.main.fragment_home.view.card_view_total_elections
-import kotlinx.android.synthetic.main.fragment_home.view.constraintLayout
-import kotlinx.android.synthetic.main.fragment_home.view.shimmer_view_container
-import kotlinx.android.synthetic.main.fragment_home.view.swipe_refresh
-import kotlinx.android.synthetic.main.fragment_home.view.text_view_active_count
-import kotlinx.android.synthetic.main.fragment_home.view.text_view_finished_count
-import kotlinx.android.synthetic.main.fragment_home.view.text_view_pending_count
-import kotlinx.android.synthetic.main.fragment_home.view.text_view_total_count
-import org.aossie.agoraandroid.R
 import org.aossie.agoraandroid.R.color
-import org.aossie.agoraandroid.R.layout
 import org.aossie.agoraandroid.data.db.PreferenceProvider
+import org.aossie.agoraandroid.databinding.FragmentHomeBinding
 import org.aossie.agoraandroid.ui.activities.main.MainActivityViewModel
 import org.aossie.agoraandroid.ui.fragments.auth.SessionExpiredListener
 import org.aossie.agoraandroid.ui.fragments.auth.login.LoginViewModel
-import org.aossie.agoraandroid.utilities.Coroutines
 import org.aossie.agoraandroid.utilities.ResponseUI
 import org.aossie.agoraandroid.utilities.snackbar
 import timber.log.Timber
@@ -58,38 +44,38 @@ constructor(
     viewModelFactory
   }
 
-  private lateinit var rootView: View
+  private lateinit var mBinding: FragmentHomeBinding
+
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    rootView = inflater.inflate(layout.fragment_home, container, false)
-
+    mBinding = FragmentHomeBinding.inflate(inflater)
     loginViewModel.sessionExpiredListener = this
-    rootView.swipe_refresh.setColorSchemeResources(color.logo_yellow, color.logo_green)
+    mBinding.swipeRefresh.setColorSchemeResources(color.logo_yellow, color.logo_green)
 
-    rootView.card_view_active_elections.setOnClickListener {
-      Navigation.findNavController(rootView)
+    mBinding.cardViewActiveElections.setOnClickListener {
+      Navigation.findNavController(mBinding.root)
         .navigate(HomeFragmentDirections.actionHomeFragmentToActiveElectionsFragment())
     }
-    rootView.card_view_pending_elections.setOnClickListener {
-      Navigation.findNavController(rootView)
+    mBinding.cardViewPendingElections.setOnClickListener {
+      Navigation.findNavController(mBinding.root)
         .navigate(HomeFragmentDirections.actionHomeFragmentToPendingElectionsFragment())
     }
-    rootView.card_view_finished_elections.setOnClickListener {
-      Navigation.findNavController(rootView)
+    mBinding.cardViewFinishedElections.setOnClickListener {
+      Navigation.findNavController(mBinding.root)
         .navigate(HomeFragmentDirections.actionHomeFragmentToFinishedElectionsFragment())
     }
-    rootView.card_view_total_elections.setOnClickListener {
-      Navigation.findNavController(rootView)
+    mBinding.cardViewTotalElections.setOnClickListener {
+      Navigation.findNavController(mBinding.root)
         .navigate(HomeFragmentDirections.actionHomeFragmentToElectionsFragment())
     }
-    rootView.button_create_election.setOnClickListener {
-      Navigation.findNavController(rootView)
+    mBinding.buttonCreateElection.setOnClickListener {
+      Navigation.findNavController(mBinding.root)
         .navigate(HomeFragmentDirections.actionHomeFragmentToCreateElectionFragment())
     }
-    rootView.swipe_refresh.setOnRefreshListener { updateUi() }
+    mBinding.swipeRefresh.setOnRefreshListener { updateUi() }
 
     loginViewModel.getLoginLiveData.observe(
       viewLifecycleOwner,
@@ -102,7 +88,7 @@ constructor(
             updateUi()
           }
           ResponseUI.Status.ERROR -> {
-            rootView.snackbar(it.message ?: "")
+            mBinding.root.snackbar(it.message ?: "")
           }
         }
       }
@@ -142,64 +128,31 @@ constructor(
           }
         }
       )
-    Coroutines.main {
-      val totalElectionCount = homeViewModel.totalElectionsCount.await()
-      val pendingElectionCount = homeViewModel.pendingElectionsCount.await()
-      val activeElectionCount = homeViewModel.activeElectionsCount.await()
-      val finishedElectionCount = homeViewModel.finishedElectionsCount.await()
-      if (view != null) {
-        homeViewModel.getElections()
-          .observe(
-            viewLifecycleOwner,
-            androidx.lifecycle.Observer {
-              totalElectionCount.observe(
-                viewLifecycleOwner,
-                Observer {
-                  rootView.text_view_total_count.text = it.toString()
-                  pendingElectionCount.observe(
-                    viewLifecycleOwner,
-                    Observer { pending ->
-                      rootView.text_view_pending_count.text = pending.toString()
-                      finishedElectionCount.observe(
-                        viewLifecycleOwner,
-                        Observer { finished ->
-                          rootView.text_view_finished_count.text = finished.toString()
-                          activeElectionCount.observe(
-                            viewLifecycleOwner,
-                            Observer { active ->
-                              rootView.text_view_active_count.text = active.toString()
-                              rootView.shimmer_view_container.stopShimmer()
-                              rootView.shimmer_view_container.visibility = View.GONE
-                              rootView.constraintLayout.visibility = View.VISIBLE
-                              rootView.swipe_refresh.isRefreshing =
-                                false // Disables the refresh icon
-                            }
-                          )
-                        }
-                      )
-                    }
-                  )
-                }
-              )
-            }
-          )
+    homeViewModel.countMediatorLiveData.observe(
+      viewLifecycleOwner,
+      {
+        mBinding.textViewActiveCount.text = it[ACTIVE_ELECTION_COUNT].toString()
+        mBinding.textViewTotalCount.text = it[TOTAL_ELECTION_COUNT].toString()
+        mBinding.textViewPendingCount.text = it[PENDING_ELECTION_COUNT].toString()
+        mBinding.textViewFinishedCount.text = it[FINISHED_ELECTION_COUNT].toString()
+        mBinding.shimmerViewContainer.stopShimmer()
+        mBinding.shimmerViewContainer.visibility = View.GONE
+        mBinding.constraintLayout.visibility = View.VISIBLE
+        mBinding.swipeRefresh.isRefreshing =
+          false // Disables the refresh icon
       }
-    }
-    return rootView
+    )
+
+    return mBinding.root
   }
 
   override fun onResume() {
     super.onResume()
-    rootView.shimmer_view_container.startShimmer()
-  }
-
-  override fun onPause() {
-    rootView.shimmer_view_container.stopShimmer()
-    super.onPause()
+    updateUi()
   }
 
   override fun onDestroyView() {
-    rootView.swipe_refresh.setOnRefreshListener(null)
+    mBinding.swipeRefresh.setOnRefreshListener(null)
     homeViewModel.sessionExpiredListener = null
     loginViewModel.sessionExpiredListener = null
     super.onDestroyView()
@@ -207,8 +160,11 @@ constructor(
 
   private fun updateUi() {
     preferenceProvider.setUpdateNeeded(true)
-    Navigation.findNavController(rootView)
-      .navigate(R.id.homeFragment)
+    homeViewModel.getElections()
+    mBinding.shimmerViewContainer.startShimmer()
+    mBinding.shimmerViewContainer.visibility = View.VISIBLE
+    mBinding.constraintLayout.visibility = View.GONE
+
   }
 
   override fun onSessionExpired() {
