f5792f2cf6501c091f1f5c2068a335a7e475b49b
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2021-08-05 00:14:27 +0530

    Klint Safe

--- app/src/main/java/org/aossie/agoraandroid/FcmService.kt
+++ app/src/main/java/org/aossie/agoraandroid/FcmService.kt
@@ -11,37 +11,36 @@ import com.google.firebase.messaging.RemoteMessage
 import org.aossie.agoraandroid.utilities.AppConstants
 import timber.log.Timber
 
-class FcmService:FirebaseMessagingService() {
-  private lateinit var notificationManager:NotificationManager
-  private lateinit var builder:Notification.Builder
+class FcmService : FirebaseMessagingService() {
+  private lateinit var notificationManager: NotificationManager
+  private lateinit var builder: Notification.Builder
   private val CHANNEl_ID = "12"
 
   override fun onMessageReceived(remoteMessage: RemoteMessage) {
     super.onMessageReceived(remoteMessage)
-    notificationManager  = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     Timber.tag("fcm").d(remoteMessage.toString())
-    remoteMessage.notification?.let {notification->
+    remoteMessage.notification?.let { notification ->
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val  notificationChannel = NotificationChannel(CHANNEl_ID, AppConstants.NOTIFICATION_DESCRIPTION, NotificationManager.IMPORTANCE_HIGH)
+        val notificationChannel = NotificationChannel(CHANNEl_ID, AppConstants.NOTIFICATION_DESCRIPTION, NotificationManager.IMPORTANCE_HIGH)
         notificationChannel.enableLights(true)
         notificationChannel.enableVibration(true)
         notificationManager.createNotificationChannel(notificationChannel)
 
-         builder = Notification.Builder(this, CHANNEl_ID)
+        builder = Notification.Builder(this, CHANNEl_ID)
           .setContentTitle(notification.title)
           .setContentText(notification.body)
           .setSmallIcon(R.mipmap.ic_launcher_round)
       } else {
 
-         builder = Notification.Builder(this)
-         .setContentTitle(notification.title)
-         .setContentText(notification.body)
-         .setSmallIcon(R.mipmap.ic_launcher_round)
+        builder = Notification.Builder(this)
+          .setContentTitle(notification.title)
+          .setContentText(notification.body)
+          .setSmallIcon(R.mipmap.ic_launcher_round)
       }
       with(NotificationManagerCompat.from(this)) {
         notify(CHANNEl_ID.toInt(), builder.build())
       }
     }
-
   }
 }
