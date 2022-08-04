a0ea4ad1cbcbe66aae114a075a72c9c0313efbb9
hackeramitkumar <amit9116260192@gmail.com>
2022-08-04 19:58:01 +0530

    added mappers for data models

---
+++ app/src/main/java/org/aossie/agoraandroid/data/mappers/AuthResponseEntityMapper.kt
@@ -0,0 +1,26 @@
+package org.aossie.agoraandroid.data.mappers
+
+import org.aossie.agoraandroid.data.network.responses.AuthResponse
+import org.aossie.agoraandroid.domain.model.AuthResponseModel
+import org.aossie.agoraandroid.utilities.EntityMapper
+
+class AuthResponseEntityMapper : EntityMapper<AuthResponse , AuthResponseModel> {
+  override fun mapFromEntity(entity: AuthResponse): AuthResponseModel {
+    return AuthResponseModel(
+      username = entity.username,
+      email = entity.email,
+      firstName = entity.firstName,
+      lastName = entity.lastName,
+      avatarURL = entity.avatarURL,
+      twoFactorAuthentication = entity.twoFactorAuthentication,
+      crypto = entity.crypto,
+      authToken = entity.authToken,
+      refreshToken = entity.refreshToken,
+      trustedDevice = entity.trustedDevice
+    )
+  }
+
+  override fun mapToEntity(domainModel: AuthResponseModel): AuthResponse {
+    TODO("Not yet implemented")
+  }
+}
--- app/src/main/java/org/aossie/agoraandroid/domain/use_cases/authentication/login/SaveUserUserCase.kt
+++ app/src/main/java/org/aossie/agoraandroid/domain/use_cases/authentication/login/SaveUserUserCase.kt
@@ -1,16 +1,18 @@
 package org.aossie.agoraandroid.domain.use_cases.authentication.login
 
-import org.aossie.agoraandroid.data.db.entities.User
+import org.aossie.agoraandroid.data.mappers.UserEntityMapper
+import org.aossie.agoraandroid.domain.model.UserModel
 import org.aossie.agoraandroid.domain.repository.UserRepository
 import javax.inject.Inject
 
 class SaveUserUseCase @Inject constructor(
   private val repository: UserRepository,
 ) {
+  private val mapper : UserEntityMapper = UserEntityMapper()
   suspend operator fun invoke(
-    user : User
+    user : UserModel
   ) {
-    return repository.saveUser(user)
+    return repository.saveUser(mapper.mapToEntity(user))
   }
 }
 
