06502af707950bf4365b3881000ed89527d3d759
hackeramitkumar <amit9116260192@gmail.com>
2022-08-08 02:06:58 +0530

    code formatting

--- app/src/main/java/org/aossie/agoraandroid/data/mappers/UpdateUserDtoEntityMapper.kt
+++ app/src/main/java/org/aossie/agoraandroid/data/mappers/UpdateUserDtoEntityMapper.kt
@@ -4,22 +4,22 @@ import org.aossie.agoraandroid.data.network.dto.UpdateUserDto
 import org.aossie.agoraandroid.domain.model.UpdateUserDtoModel
 import org.aossie.agoraandroid.utilities.EntityMapper
 
-class UpdateUserDtoEntityMapper : EntityMapper<UpdateUserDto , UpdateUserDtoModel> {
+class UpdateUserDtoEntityMapper : EntityMapper<UpdateUserDto, UpdateUserDtoModel> {
   override fun mapFromEntity(entity: UpdateUserDto): UpdateUserDtoModel {
     TODO("Not yet implemented")
   }
 
   override fun mapToEntity(domainModel: UpdateUserDtoModel): UpdateUserDto {
-     return UpdateUserDto(
-       identifier = domainModel.identifier,
-       email = domainModel.email,
-       firstName = domainModel.firstName,
-       lastName = domainModel.lastName,
-       trustedDevice = domainModel.trustedDevice,
-       twoFactorAuthentication = domainModel.twoFactorAuthentication,
-       avatarURL = domainModel.avatarURL,
-       authToken = domainModel.authToken,
-       refreshToken = domainModel.refreshToken
-     )
+    return UpdateUserDto(
+      identifier = domainModel.identifier,
+      email = domainModel.email,
+      firstName = domainModel.firstName,
+      lastName = domainModel.lastName,
+      trustedDevice = domainModel.trustedDevice,
+      twoFactorAuthentication = domainModel.twoFactorAuthentication,
+      avatarURL = domainModel.avatarURL,
+      authToken = domainModel.authToken,
+      refreshToken = domainModel.refreshToken
+    )
   }
 }
