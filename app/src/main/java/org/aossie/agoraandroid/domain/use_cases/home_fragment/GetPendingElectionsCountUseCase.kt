c92b3757a855085c59ae1c0f2a3a0579190d0a3e
hackeramitkumar <amit9116260192@gmail.com>
2022-08-10 19:52:17 +0530

    added home fragment use cases

---
+++ app/src/main/java/org/aossie/agoraandroid/domain/use_cases/home_fragment/GetPendingElectionsCountUseCase.kt
@@ -0,0 +1,15 @@
+package org.aossie.agoraandroid.domain.use_cases.home_fragment
+
+import androidx.lifecycle.LiveData
+import org.aossie.agoraandroid.domain.repository.ElectionsRepository
+import javax.inject.Inject
+
+class GetPendingElectionsCountUseCase @Inject constructor(
+  private val electionsRepository: ElectionsRepository
+) {
+  suspend operator fun invoke(
+    date: String
+  ): LiveData<Int> {
+    return electionsRepository.getPendingElectionsCount(date)
+  }
+}
