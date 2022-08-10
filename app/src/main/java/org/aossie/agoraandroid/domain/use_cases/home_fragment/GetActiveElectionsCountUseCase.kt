e2f7fd59e321c189bb3fa9649b1a966682edc5c4
hackeramitkumar <amit9116260192@gmail.com>
2022-08-10 19:52:17 +0530

    added home fragment use cases

--- app/src/main/java/org/aossie/agoraandroid/domain/use_cases/home_fragment/GetActiveElectionsCountUseCase.kt
+++ app/src/main/java/org/aossie/agoraandroid/domain/use_cases/home_fragment/GetActiveElectionsCountUseCase.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 class GetActiveElectionsCountUseCase @Inject constructor(
   private val electionsRepository: ElectionsRepository
 ) {
-  operator fun invoke(
+  suspend operator fun invoke(
     date: String
   ): LiveData<Int> {
     return electionsRepository.getActiveElectionsCount(date)
