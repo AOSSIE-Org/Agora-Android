14612df6538e499faedad09f8057e4c7c68ffd2b
hackeramitkumar <amit9116260192@gmail.com>
2022-08-13 16:24:27 +0530

    Added use cases for election details fragments

---
+++ app/src/main/java/org/aossie/agoraandroid/domain/use_cases/election_details/GetElectionByIdUseCase.kt
@@ -0,0 +1,18 @@
+package org.aossie.agoraandroid.domain.use_cases.election_details
+
+import androidx.lifecycle.LiveData
+import org.aossie.agoraandroid.data.mappers.ElectionEntityMapper
+import org.aossie.agoraandroid.domain.model.ElectionModel
+import org.aossie.agoraandroid.domain.repository.ElectionsRepository
+import javax.inject.Inject
+
+class GetElectionByIdUseCase @Inject constructor(
+  private val electionsRepository: ElectionsRepository
+) {
+  private val electionEntityMapper = ElectionEntityMapper()
+  operator fun invoke(
+    id: String
+  ): LiveData<ElectionModel> {
+    return electionEntityMapper.mapFromEntityLiveData(electionsRepository.getElectionById(id))
+  }
+}
