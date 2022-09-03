c04d72464661b0892a2ac74d3f3142153a33cc72
hackeramitkumar <amit9116260192@gmail.com>
2022-09-03 11:18:57 +0530

    election repository flow implementation

--- app/src/main/java/org/aossie/agoraandroid/domain/use_cases/election_details/ElectionDetailsUseCases.kt
+++
@@ -1,9 +0,0 @@
-package org.aossie.agoraandroid.domain.use_cases.election_details
-
-data class ElectionDetailsUseCases(
-  val deleteElectionUseCase: DeleteElectionUseCase,
-  val getBallotsUseCase: GetBallotsUseCase,
-  val getElectionByIdUseCase: GetElectionByIdUseCase,
-  val getResultUseCase: GetResultUseCase,
-  val getVotersUseCase: GetVotersUseCase
-)
