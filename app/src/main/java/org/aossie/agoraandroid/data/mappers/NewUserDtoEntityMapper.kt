55aa9b91fb21efe550dc4493e65512d265fc7ad3
hackeramitkumar <amit9116260192@gmail.com>
2022-09-04 00:25:05 +0530

    removed todo

--- app/src/main/java/org/aossie/agoraandroid/data/mappers/NewUserDtoEntityMapper.kt
+++ app/src/main/java/org/aossie/agoraandroid/data/mappers/NewUserDtoEntityMapper.kt
@@ -2,14 +2,9 @@ package org.aossie.agoraandroid.data.mappers
 
 import org.aossie.agoraandroid.data.network.dto.NewUserDto
 import org.aossie.agoraandroid.domain.model.NewUserDtoModel
-import org.aossie.agoraandroid.utilities.EntityMapper
 
-class NewUserDtoEntityMapper : EntityMapper<NewUserDto, NewUserDtoModel> {
-  override fun mapFromEntity(entity: NewUserDto): NewUserDtoModel {
-    TODO("Not yet implemented")
-  }
-
-  override fun mapToEntity(domainModel: NewUserDtoModel): NewUserDto {
+class NewUserDtoEntityMapper {
+  fun mapToEntity(domainModel: NewUserDtoModel): NewUserDto {
     return NewUserDto(
       email = domainModel.email,
       firstName = domainModel.firstName,
--- app/src/main/java/org/aossie/agoraandroid/data/mappers/WinnerDtoEntityMapper.kt
+++ app/src/main/java/org/aossie/agoraandroid/data/mappers/WinnerDtoEntityMapper.kt
@@ -2,18 +2,13 @@ package org.aossie.agoraandroid.data.mappers
 
 import org.aossie.agoraandroid.data.network.dto.WinnerDto
 import org.aossie.agoraandroid.domain.model.WinnerDtoModel
-import org.aossie.agoraandroid.utilities.EntityMapper
 
-class WinnerDtoEntityMapper : EntityMapper<WinnerDto, WinnerDtoModel> {
-  override fun mapFromEntity(entity: WinnerDto): WinnerDtoModel {
+class WinnerDtoEntityMapper {
+  fun mapFromEntity(entity: WinnerDto): WinnerDtoModel {
     return WinnerDtoModel(
       candidate = entity.candidate,
       score = entity.score
     )
-  }
-
-  override fun mapToEntity(domainModel: WinnerDtoModel): WinnerDto {
-    TODO("Not yet implemented")
   }
 
   fun mapFromEntityList(entityList: List<WinnerDto>?): List<WinnerDtoModel>? {
