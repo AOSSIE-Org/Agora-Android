f3932210258a9d92e438a5cb609c0bf933ae9e2a
hackeramitkumar <amit9116260192@gmail.com>
2022-09-04 11:54:08 +0530

    code foramatting

--- app/src/main/java/org/aossie/agoraandroid/domain/useCases/electionDetails/GetResultUseCase.kt
+++ app/src/main/java/org/aossie/agoraandroid/domain/useCases/electionDetails/GetResultUseCase.kt
@@ -1,7 +1,6 @@
 package org.aossie.agoraandroid.domain.useCases.electionDetails
 
 import org.aossie.agoraandroid.data.mappers.Mappers
-import org.aossie.agoraandroid.data.mappers.WinnerDtoEntityMapper
 import org.aossie.agoraandroid.domain.model.WinnerDtoModel
 import org.aossie.agoraandroid.domain.repository.ElectionsRepository
 import javax.inject.Inject
