cdd309da6d0c1ebefb543be9e6b706130f406a9a
hackeramitkumar <amit9116260192@gmail.com>
2022-09-07 21:10:13 +0530

    removed underscore from pakage name

--- app/src/main/java/org/aossie/agoraandroid/domain/useCases/castVoteActivity/VerifyVotersUseCase.kt
+++ app/src/main/java/org/aossie/agoraandroid/domain/useCases/castVoteActivity/VerifyVotersUseCase.kt
@@ -1,17 +1,17 @@
 package org.aossie.agoraandroid.domain.useCases.castVoteActivity
 
-import org.aossie.agoraandroid.data.mappers.Mappers
+import org.aossie.agoraandroid.data.mappers.ElectionDtoEntityMapper
 import org.aossie.agoraandroid.domain.model.ElectionDtoModel
 import org.aossie.agoraandroid.domain.repository.ElectionsRepository
 import javax.inject.Inject
 
 class VerifyVotersUseCase @Inject constructor(
-  private val electionsRepository: ElectionsRepository,
-  private val mappers: Mappers
+  private val electionsRepository: ElectionsRepository
 ) {
+  private val electionDtoEntityMapper = ElectionDtoEntityMapper()
   suspend operator fun invoke(
     id: String
   ): ElectionDtoModel {
-    return mappers.electionDtoEntityMapper.mapFromEntity(electionsRepository.verifyVoter(id))
+    return electionDtoEntityMapper.mapFromEntity(electionsRepository.verifyVoter(id))
   }
 }
