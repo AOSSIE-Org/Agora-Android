96e6eace2dcd594e8cbdcb23587ebca32991eab0
hackeramitkumar <amit9116260192@gmail.com>
2022-09-10 15:52:53 +0530

    removed extra unused import

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/twoFactorAuthentication/TwoFactorAuthFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/twoFactorAuthentication/TwoFactorAuthFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
