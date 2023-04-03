ab82ba878a22d05d7d34a345979609c4fbea44ad
abhaysood1793 <abhayasood99@gmail.com>
2023-04-04 05:15:29 +0530

    Fixed the empty election textview visibility issue even when list is not empty

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/ElectionsFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/ElectionsFragment.kt
@@ -98,7 +98,7 @@ constructor(
   private fun filter(query: String) {
     val updatedList = electionViewModel.filter(mElections, query)
     electionsAdapter.submitList(updatedList)
-    if (updatedList.isEmpty()) {
+    if (updatedList.isEmpty() && binding.searchView.isFocused) {
       binding.tvEmptyElection.show()
     } else {
       binding.tvEmptyElection.hide()
