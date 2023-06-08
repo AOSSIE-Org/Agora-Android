f6bcc66971e731aa81264fddc21187dc9c44997f
Narendra Singh Anjana <narendraanjana09@gmail.com>
2023-06-08 14:07:02 +0530

    Migrated fragment_login.xml to LoginScreen.kt Compose and Updated facebook Sdk version to latest and added code changes according to new methods in doc and updated gitlab-ci.yml for fbClientSecret

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/login/LoginFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/login/LoginFragment.kt
@@ -1,34 +1,44 @@
 package org.aossie.agoraandroid.ui.fragments.auth.login
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doAfterTextChanged
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.facebook.CallbackManager
-import com.facebook.CallbackManager.Factory
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.aossie.agoraandroid.R
 import org.aossie.agoraandroid.data.db.PreferenceProvider
-import org.aossie.agoraandroid.databinding.FragmentLoginBinding
 import org.aossie.agoraandroid.ui.fragments.BaseFragment
-import org.aossie.agoraandroid.utilities.HideKeyboard
-import org.aossie.agoraandroid.utilities.ResponseUI
-import org.aossie.agoraandroid.utilities.disableView
-import org.aossie.agoraandroid.utilities.enableView
-import org.aossie.agoraandroid.utilities.hide
-import org.aossie.agoraandroid.utilities.show
-import org.aossie.agoraandroid.utilities.toggleIsEnable
+import org.aossie.agoraandroid.ui.screens.auth.login.LoginScreen
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginScreenEvent.BackArrowClick
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginScreenEvent.EnteredPassword
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginScreenEvent.EnteredUsername
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginScreenEvent.ForgotPasswordClick
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginScreenEvent.LoginClick
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginScreenEvent.LoginFacebookClick
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginScreenEvent.SnackBarActionClick
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginUiEvent.OnTwoFactorAuthentication
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginUiEvent.UserLoggedIn
+import org.aossie.agoraandroid.ui.screens.auth.login.events.LoginViewModelEvent
+import org.aossie.agoraandroid.ui.theme.AgoraTheme
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -41,7 +51,7 @@ constructor(
   private val viewModelFactory: ViewModelProvider.Factory,
   private val prefs: PreferenceProvider
 ) : BaseFragment(viewModelFactory) {
-  lateinit var binding: FragmentLoginBinding
+  private lateinit var composeView: ComposeView
 
   private val loginViewModel: LoginViewModel by viewModels {
     viewModelFactory
@@ -53,42 +63,58 @@ constructor(
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    binding = FragmentLoginBinding.inflate(inflater)
-    return binding.root
+    return ComposeView(requireContext()).also {
+      composeView = it
+    }
   }
 
+  @OptIn(ExperimentalMaterial3Api::class)
   override fun onFragmentInitiated() {
+    composeView.setContent {
 
-    initObjects()
+      val loginDataState by loginViewModel.loginDataState.collectAsState()
+      val progressErrorState by loginViewModel.progressAndErrorState.collectAsState()
 
-    initListeners()
-  }
+      val systemUiController = rememberSystemUiController()
+      val useDarkIcons = !isSystemInDarkTheme()
 
-  private fun initObjects() {
-    loginViewModel.sessionExpiredListener = this
-    lifecycleScope.launch {
-      loginViewModel.getLoginStateFlow.collect {
-        if (it != null) {
-          when (it.status) {
-            ResponseUI.Status.LOADING -> {
-              binding.progressBar.show()
-              makeFieldsToggleEnable()
+      DisposableEffect(systemUiController, useDarkIcons) {
+        systemUiController.setStatusBarColor(
+          color = Color.Transparent,
+          darkIcons = useDarkIcons
+        )
+        onDispose {}
+      }
+      AgoraTheme {
+        LoginScreen(loginDataState,progressErrorState) { event ->
+          when(event){
+            is EnteredPassword -> {
+              loginViewModel.onEvent(LoginViewModelEvent.EnteredPassword(event.password))
             }
-            ResponseUI.Status.SUCCESS -> {
-              binding.progressBar.hide()
-              makeFieldsToggleEnable()
-              it.message?.let { crypto ->
-                onTwoFactorAuthentication(crypto)
-              } ?: kotlin.run {
-                Navigation.findNavController(binding.root)
-                  .navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
-              }
+            is EnteredUsername -> {
+              loginViewModel.onEvent(LoginViewModelEvent.EnteredUserName(event.username))
             }
-            ResponseUI.Status.ERROR -> {
-              binding.progressBar.hide()
-              notify(it.message)
-              makeFieldsToggleEnable()
-              enableBtnFacebook()
+            ForgotPasswordClick -> {
+              findNavController()
+                .navigate(LoginFragmentDirections.actionLoginFragmentToForgotPasswordFragment())
+            }
+            LoginClick -> {
+              loginViewModel.onEvent(LoginViewModelEvent.LoginClick)
+            }
+            BackArrowClick ->{
+              findNavController().navigateUp()
+            }
+            LoginFacebookClick -> {
+              LoginManager
+                .getInstance()
+                .logInWithReadPermissions(
+                  requireActivity(),
+                  callbackManager!!,
+                  listOf("email", "public_profile")
+                )
+            }
+            SnackBarActionClick -> {
+              loginViewModel.hideSnackBar()
             }
             else -> {}
           }
@@ -96,13 +122,35 @@ constructor(
       }
     }
 
-    callbackManager = Factory.create()
+    initObjects()
+
+    initListeners()
+  }
+
+  private fun initListeners() {
+    lifecycleScope.launch {
+      loginViewModel.uiEvents.collectLatest { event ->
+        when(event) {
+          UserLoggedIn -> {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
+          }
+          is OnTwoFactorAuthentication -> {
+            onTwoFactorAuthentication(event.crypto)
+          }
+        }
+      }
+    }
+  }
+
+  private fun initObjects() {
+    loginViewModel.sessionExpiredListener = this
+    callbackManager = CallbackManager.Factory.create()
 
     LoginManager.getInstance()
       .registerCallback(
         callbackManager,
-        object : FacebookCallback<LoginResult?> {
-          override fun onSuccess(loginResult: LoginResult?) {
+        object : FacebookCallback<LoginResult> {
+          override fun onSuccess(loginResult: LoginResult) {
             Timber.d("Success")
             lifecycleScope.launch {
               prefs.setFacebookAccessToken(loginResult?.accessToken?.token)
@@ -111,85 +159,19 @@ constructor(
           }
 
           override fun onCancel() {
-            enableBtnFacebook()
-            notify(resources.getString(R.string.login_cancelled))
+            loginViewModel.showError(resources.getString(R.string.login_cancelled))
           }
 
           override fun onError(exception: FacebookException) {
-            enableBtnFacebook()
             notify(exception.message.toString())
+            loginViewModel.showError(exception.message.toString())
           }
         }
       )
   }
 
-  private fun makeFieldsToggleEnable() {
-    binding.loginBtn.toggleIsEnable()
-    binding.username.toggleIsEnable()
-    binding.password.toggleIsEnable()
-  }
-
-  private fun initListeners() {
-    binding.forgotPasswordTv.setOnClickListener {
-      Navigation.findNavController(binding.root)
-        .navigate(LoginFragmentDirections.actionLoginFragmentToForgotPasswordFragment())
-    }
-
-    binding.loginBtn.setOnClickListener {
-      val userName = binding.loginUserNameTil.editText
-        ?.text
-        .toString()
-        .trim { it <= ' ' }
-      val userPass = binding.loginPasswordTil.editText
-        ?.text
-        .toString()
-        .trim { it <= ' ' }
-      HideKeyboard.hideKeyboardInFrag(this)
-      loginViewModel.logInRequest(userName, userPass)
-    }
-
-    binding.password.doAfterTextChanged {
-      updateLoginButton()
-    }
-    binding.username.doAfterTextChanged {
-      updateLoginButton()
-    }
-
-    binding.btnFacebookLogin.setOnClickListener {
-      binding.btnFacebookLogin.disableView()
-      LoginManager.getInstance()
-        .logInWithReadPermissions(
-          activity,
-          listOf("email", "public_profile")
-        )
-    }
-  }
-
-  private fun updateLoginButton() {
-    val usernameInput: String = binding.username.text
-      .toString()
-      .trim()
-    val passwordInput: String = binding.password.text
-      .toString()
-      .trim()
-    binding.loginBtn.isEnabled = usernameInput.isNotEmpty() && passwordInput.isNotEmpty()
-  }
-
-  override fun onActivityResult(
-    requestCode: Int,
-    resultCode: Int,
-    data: Intent?
-  ) {
-    Timber.d("Activity result")
-    callbackManager!!.onActivityResult(requestCode, resultCode, data)
-  }
-
   override fun onSessionExpired() {
     // do nothing
-  }
-
-  private fun enableBtnFacebook() {
-    binding.btnFacebookLogin.enableView()
   }
 
   fun onTwoFactorAuthentication(
@@ -201,10 +183,8 @@ constructor(
           if (it != null) {
             if (it.twoFactorAuthentication!!) {
               notify(getString(R.string.otp_sent))
-              val action =
-                LoginFragmentDirections.actionLoginFragmentToTwoFactorAuthFragment(crypto)
-              Navigation.findNavController(binding.root)
-                .navigate(action)
+              val action = LoginFragmentDirections.actionLoginFragmentToTwoFactorAuthFragment(crypto)
+              findNavController().navigate(action)
             }
           }
         }
---
+++ app/src/main/java/org/aossie/agoraandroid/ui/screens/auth/login/events/LoginScreenEvent.kt
@@ -0,0 +1,12 @@
+package org.aossie.agoraandroid.ui.screens.auth.login.events
+
+sealed class LoginScreenEvent{
+  data class EnteredUsername(val username:String): LoginScreenEvent()
+  data class EnteredPassword(val password:String): LoginScreenEvent()
+  object BackArrowClick: LoginScreenEvent()
+  object SnackBarActionClick: LoginScreenEvent()
+  object LoginClick: LoginScreenEvent()
+  object LoginFacebookClick: LoginScreenEvent()
+  object ForgotPasswordClick: LoginScreenEvent()
+
+}
