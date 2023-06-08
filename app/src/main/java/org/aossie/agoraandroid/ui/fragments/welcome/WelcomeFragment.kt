134e179d217e5173a254a97237b15047735eb495
Narendra Singh Anjana <narendraanjana09@gmail.com>
2023-06-08 14:07:02 +0530

    Migrated fragment_welcome.xml and fragment_intro.xml to compose under WelcomeScreen.kt

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/welcome/WelcomeFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/welcome/WelcomeFragment.kt
@@ -4,24 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.navigation.fragment.findNavController
 import org.aossie.agoraandroid.R
-import org.aossie.agoraandroid.data.adapters.IntroViewpagerAdapter
-import org.aossie.agoraandroid.databinding.FragmentWelcomeBinding
+import org.aossie.agoraandroid.ui.screens.welcome.WelcomeScreen
+import org.aossie.agoraandroid.ui.screens.welcome.WelcomeScreenEvent.ALREADY_HAVE_ACCOUNT_CLICKED
+import org.aossie.agoraandroid.ui.screens.welcome.WelcomeScreenEvent.GET_STARTED_CLICKED
+import org.aossie.agoraandroid.ui.theme.AgoraTheme
 
 /**
  * A simple [Fragment] subclass.
  */
 class WelcomeFragment : Fragment() {
 
-  private var binding: FragmentWelcomeBinding? = null
+  private lateinit var composeView:ComposeView
+
   private val data: Array<Pair<Int, Int>> = arrayOf(
-    Pair(R.string.first_welcome_message, R.drawable.boy_ship),
-    Pair(R.string.second_welcome_message, R.drawable.election),
-    Pair(R.string.third_welcome_message, R.drawable.vote),
-    Pair(R.string.fourth_welcome_message, R.drawable.visualize)
+    Pair(R.string.first_welcome_message, R.drawable.img_empowering_democracy),
+    Pair(R.string.second_welcome_message, R.drawable.img_create_schedule_election),
+    Pair(R.string.third_welcome_message, R.drawable.img_invite_voters_elect),
+    Pair(R.string.fourth_welcome_message, R.drawable.img_visualize_results)
   )
 
   override fun onCreateView(
@@ -29,27 +36,31 @@ class WelcomeFragment : Fragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    // Inflate the layout for this fragment
-    binding = FragmentWelcomeBinding.inflate(layoutInflater)
-    binding?.let { _binding ->
-      _binding.viewPager.adapter = IntroViewpagerAdapter(this, data)
-      TabLayoutMediator(_binding.tabLayout, _binding.viewPager) { _, _ ->
-      }.attach()
-
-      _binding.btnLogin.setOnClickListener {
-        Navigation.findNavController(_binding.root)
-          .navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment())
-      }
-      _binding.btnSignup.setOnClickListener {
-        Navigation.findNavController(_binding.root)
-          .navigate(WelcomeFragmentDirections.actionWelcomeFragmentToSignUpFragment())
-      }
+    return ComposeView(requireContext()).also {
+      composeView = it
     }
-    return binding?.root
   }
 
-  override fun onDestroyView() {
-    super.onDestroyView()
-    binding = null
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    composeView.setContent {
+      AgoraTheme {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+          WelcomeScreen(data){
+            when(it){
+              ALREADY_HAVE_ACCOUNT_CLICKED -> {
+                findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment())
+              }
+              GET_STARTED_CLICKED -> {
+                findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToSignUpFragment())
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
