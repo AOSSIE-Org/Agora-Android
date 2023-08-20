c6c4436b6ce129745c8f3810afc86abfb590cc5e
Narendra Singh Anjana <narendraanjana09@gmail.com>
2023-08-20 21:50:31 +0530

    Migrated fragment_welcome.xml and fragment_intro.xml to compose under WelcomeScreen.kt (#11)

--- app/src/main/java/org/aossie/agoraandroid/ui/screens/common/Util/ScreenState.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/screens/common/Util/ScreenState.kt
@@ -1,7 +1,6 @@
 package org.aossie.agoraandroid.ui.screens.common.Util
 
 data class ScreensState(
-  val isLoading: Pair<String,Boolean> = Pair("",false),
-  val error: Pair<String,Boolean> = Pair("",false),
-  val errorResource: Pair<Int,Boolean> = Pair(0,false),
+  val loading: Pair<Any,Boolean> = Pair("",false),
+  val message: Pair<Any,Boolean> = Pair("",false),
 )
