ad6df0fc049f24e8083083b6057fc50dfff86083
Narendra Singh Anjana <narendraanjana09@gmail.com>
2023-08-30 01:43:09 +0530

    Gsoc 2023 migrating elections fragment to compose (#31)
    * Update for some issue In Deploy to Appetize
    
    * removed Github Token from ci file
    
    * Migrated fragment_elections.xml.xml to ElectionsScreen.kt in compose

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/ElectionsFragment.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/elections/ElectionsFragment.kt
@@ -4,20 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doAfterTextChanged
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.launch
-import org.aossie.agoraandroid.data.adapters.ElectionsAdapter
-import org.aossie.agoraandroid.databinding.FragmentElectionsBinding
-import org.aossie.agoraandroid.domain.model.ElectionModel
+import androidx.navigation.fragment.findNavController
 import org.aossie.agoraandroid.ui.fragments.BaseFragment
-import org.aossie.agoraandroid.utilities.hide
-import org.aossie.agoraandroid.utilities.show
+import org.aossie.agoraandroid.ui.screens.elections.ElectionsScreen
+import org.aossie.agoraandroid.ui.theme.AgoraTheme
 import javax.inject.Inject
 
 class ElectionsFragment
@@ -30,78 +25,48 @@ constructor(
     viewModelFactory
   }
 
-  lateinit var mElections: ArrayList<ElectionModel>
-  private lateinit var electionsAdapter: ElectionsAdapter
-
   private val onItemClicked = { _id: String ->
     val action = ElectionsFragmentDirections
       .actionElectionsFragmentToElectionDetailsFragment(_id)
-    Navigation.findNavController(binding.root)
-      .navigate(action)
+    findNavController().navigate(action)
   }
-  private lateinit var binding: FragmentElectionsBinding
+  private lateinit var composeView: ComposeView
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    binding = FragmentElectionsBinding.inflate(inflater)
-    return binding.root
+    return ComposeView(requireContext()).also {
+      composeView = it
+    }
   }
 
   override fun onFragmentInitiated() {
-    mElections = ArrayList()
-    electionsAdapter = ElectionsAdapter(onItemClicked)
-    binding.rvTotalElections.apply {
-      layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-      adapter = electionsAdapter
-    }
-    binding.searchView.doAfterTextChanged {
-      filter(it.toString())
-    }
-  }
-
-  override fun onNetworkConnected() {
     bindUI()
-  }
-
-  override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-    bindUI()
-  }
-
-  private fun bindUI() {
-    lifecycleScope.launch {
-      try {
-        electionViewModel.getElections()
-          .collect {
-            if (it != null) {
-              addElections(it)
-            }
-          }
-      } catch (e: IllegalStateException) {
-        binding.tvSomethingWentWrong.show()
+    composeView.setContent {
+      val elections by electionViewModel.elections.collectAsState()
+      val progressErrorState by electionViewModel.progressAndErrorState.collectAsState()
+      val searchText by electionViewModel.search
+      AgoraTheme {
+        ElectionsScreen(
+          screenState = progressErrorState,
+          elections = elections,
+          searchText = searchText,
+          onSearch = {
+            electionViewModel.getElectionsState(it)
+          },
+          onItemClicked = onItemClicked
+        )
       }
     }
   }
 
-  private fun addElections(elections: List<ElectionModel>) {
-    if (elections.isNotEmpty()) {
-      mElections.addAll(elections)
-      electionsAdapter.submitList(elections)
-    } else {
-      binding.tvEmptyElection.show()
-    }
+  private fun bindUI() {
+    electionViewModel.getElectionsState(electionViewModel.search.value)
   }
 
-  private fun filter(query: String) {
-    val updatedList = electionViewModel.filter(mElections, query)
-    electionsAdapter.submitList(updatedList)
-    if (updatedList.isEmpty()) {
-      binding.tvEmptyElection.show()
-    } else {
-      binding.tvEmptyElection.hide()
-    }
+  override fun onNetworkConnected() {
+    bindUI()
   }
 }
