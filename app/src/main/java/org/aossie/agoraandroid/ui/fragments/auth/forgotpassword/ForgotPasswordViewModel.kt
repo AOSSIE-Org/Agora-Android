a51bfd90a2666fd4f0b31f0d59580e16b52f8fae
Narendra Singh Anjana <narendraanjana09@gmail.com>
2023-08-29 22:05:15 +0530

    Gsoc 2023 forgot password screen migration (#63)
    * Update for some issue In Deploy to Appetize
    
    * removed Github Token from ci file
    
    * Forgot Password Screen Migration to jetpack compose

--- app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/forgotpassword/ForgotPasswordViewModel.kt
+++ app/src/main/java/org/aossie/agoraandroid/ui/fragments/auth/forgotpassword/ForgotPasswordViewModel.kt
@@ -2,14 +2,20 @@ package org.aossie.agoraandroid.ui.fragments.auth.forgotpassword
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import org.aossie.agoraandroid.R
 import org.aossie.agoraandroid.domain.useCases.authentication.forgotPassword.SendForgotPasswordLinkUseCase
+import org.aossie.agoraandroid.ui.screens.auth.forgotPassword.ForgotPasswordScreenEvents
+import org.aossie.agoraandroid.ui.screens.auth.forgotPassword.ForgotPasswordScreenEvents.OnSendLinkClick
+import org.aossie.agoraandroid.ui.screens.auth.forgotPassword.ForgotPasswordScreenEvents.OnUserNameEntered
+import org.aossie.agoraandroid.ui.screens.auth.forgotPassword.ForgotPasswordScreenEvents.SnackBarActionClick
+import org.aossie.agoraandroid.ui.screens.common.Util.ScreensState
 import org.aossie.agoraandroid.utilities.ApiException
 import org.aossie.agoraandroid.utilities.AppConstants
 import org.aossie.agoraandroid.utilities.NoInternetException
-import org.aossie.agoraandroid.utilities.ResponseUI
 import javax.inject.Inject
 
 class ForgotPasswordViewModel
@@ -18,26 +24,78 @@ constructor(
   private val sendForgotPasswordLinkUseCase: SendForgotPasswordLinkUseCase
 ) : ViewModel() {
 
-  private val _getSendResetLinkStateFlow: MutableStateFlow<ResponseUI<Any>?> =
-    MutableStateFlow(null)
-  val getSendResetLinkStateFlow: StateFlow<ResponseUI<Any>?> =
-    _getSendResetLinkStateFlow
+  private val _userNameState = MutableStateFlow ("")
+  val userNameState = _userNameState.asStateFlow()
 
-  fun sendResetLink(userName: String?) = viewModelScope.launch {
-    _getSendResetLinkStateFlow.value = ResponseUI.loading()
+  private val _progressAndErrorState = MutableStateFlow (ScreensState())
+  val progressAndErrorState = _progressAndErrorState.asStateFlow()
+
+
+ private fun sendResetLink(userName: String?) = viewModelScope.launch {
+   showLoading("Sending link...")
     try {
       sendForgotPasswordLinkUseCase(userName)
-      _getSendResetLinkStateFlow.value = ResponseUI.success()
+      hideLoading()
+      showMessage(R.string.link_sent_please_check_your_email)
     } catch (e: ApiException) {
       if (e.message == "412") {
-        _getSendResetLinkStateFlow.value = ResponseUI.error(AppConstants.INVALID_USERNAME_MESSAGE)
+        showMessage(AppConstants.INVALID_USERNAME_MESSAGE)
       } else {
-        _getSendResetLinkStateFlow.value = ResponseUI.error(e.message)
+        showMessage(e.message!!)
       }
     } catch (e: NoInternetException) {
-      _getSendResetLinkStateFlow.value = ResponseUI.error(e.message)
+      showMessage(e.message!!)
     } catch (e: Exception) {
-      _getSendResetLinkStateFlow.value = ResponseUI.error(e.message)
+      showMessage(e.message!!)
     }
+  }
+
+  fun onEvent(forgotPasswordScreenEvents: ForgotPasswordScreenEvents){
+    when(forgotPasswordScreenEvents){
+      OnSendLinkClick -> {
+        val userName = _userNameState.value.trim()
+        if (userName.isEmpty()) {
+          showMessage("Please Enter User Name")
+        } else {
+          sendResetLink(userName)
+        }
+      }
+      is OnUserNameEntered -> {
+        _userNameState.value = forgotPasswordScreenEvents.userName
+      }
+      SnackBarActionClick -> {
+        hideSnackBar()
+      }
+      else -> {}
+    }
+  }
+
+  private fun showLoading(message: Any) {
+    _progressAndErrorState.value = progressAndErrorState.value.copy(
+      loading = Pair(message,true)
+    )
+  }
+
+  fun showMessage(message: Any) {
+    _progressAndErrorState.value = progressAndErrorState.value.copy(
+      message = Pair(message,true),
+      loading = Pair("",false)
+    )
+    viewModelScope.launch {
+      delay(AppConstants.SNACKBAR_DURATION)
+      hideSnackBar()
+    }
+  }
+
+  private fun hideSnackBar() {
+    _progressAndErrorState.value = progressAndErrorState.value.copy(
+      message = Pair("",false)
+    )
+  }
+
+  private fun hideLoading() {
+    _progressAndErrorState.value = progressAndErrorState.value.copy(
+      loading = Pair("",false)
+    )
   }
 }
